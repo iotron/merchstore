15b35f2b608e9e2736dff463826e4fc664e4ad6d
Krishanu <36711704+krishzzi@users.noreply.github.com>
2023-08-11 20:30:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
+import { defineConfig } from 'vite'
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
+// filament css remove: error happen in AppServiceProvider Filament Vite setup
+// 'resources/css/filament.css',
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ['resources/css/app.css',  'resources/js/app.js'],
+            refresh: [...refreshPaths, 'app/Http/Livewire/**'],
         }),
     ],
-});
+})
