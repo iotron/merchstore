86e3ed016eb52eaa0237542d9c2b9505fe8fb8ea
Krishanu <36711704+krishzzi@users.noreply.github.com>
2023-09-01 19:17:41 +0530

    Update cart.php

--- routes/ver/cart.php
+++ routes/ver/cart.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('cart', CartController::class, [
     'parameters' => [
-        'cart' => 'product'
+        'cart' => 'product:url'
     ]
 ]);
 
-Route::post('cart/add/{product}',[CartController::class,'add']);
+Route::post('cart/add/{product:url}',[CartController::class,'add']);
 Route::post('cart/coupon/apply', [CartController::class, 'applyCouponCode']);
 Route::delete('cart/coupon/delete', [CartController::class, 'removeCouponCode']);
