1a42c0ba14a38a9abdcb3fb2e2094fa63b749779
PrathameshPawar119 <cm.b26.prathameshpawar@gmail.com>
2023-10-08 19:17:49 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             SaleSeeder::class,
             VoucherSeeder::class,
+            ProductFeedbackSeeder::class
         ]);
     }
 }
