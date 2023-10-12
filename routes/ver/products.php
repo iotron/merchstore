38edac02760cb26a22f5e4fd2b89771184291ece
PrathameshPawar119 <cm.b26.prathameshpawar@gmail.com>
2023-10-12 21:06:46 +0530

    getPopularProducts at HomeController

--- routes/ver/products.php
+++ routes/ver/products.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',[\App\Http\Controllers\Api\ProductController::class,'index']);
+Route::get('/popular', [\App\Http\Controllers\Api\HomeController::class, 'getPopularProducts']);
 Route::get('{product:url}',[\App\Http\Controllers\Api\ProductController::class,'show']);
 Route::get('{category:url}/category',[\App\Http\Controllers\Api\ProductController::class,'showProductsByCategory']);
 Route::get('{theme:url}/theme',[\App\Http\Controllers\Api\ProductController::class,'showProductsByTheme']);
