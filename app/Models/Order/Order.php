273a3d949b4d9c52a6a3a41b579be1e1c57ed3b0
Krishanu <36711704+krishzzi@users.noreply.github.com>
2023-11-17 17:18:12 +0530

    Update Order.php

--- app/Models/Order/Order.php
+++ app/Models/Order/Order.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'order_receipt',
+        'amount',
+        'subtotal',
+        'discount_amount',
+        'tax_amount',
+        'total',
+        'quantity',
+        'voucher',
+        'tracking_id',
+        'status',
+        'payment_success',
+        'expire_at',
+        'customer_id',
+        'payment_provider_id',
+        'customer_gstin',
+        'shipping_is_billing',
+        'billing_address_id',
+        'address_id',
+    ];
+
+
+
 }
