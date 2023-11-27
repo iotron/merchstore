1598a361533e4b87598dcb198a3104e717109361
Krishanu <36711704+krishzzi@users.noreply.github.com>
2023-11-27 22:23:47 +0530

    Update ShipRocketApi.php
    fix shiprocket service provider api

--- app/Services/ShippingService/Providers/ShipRocket/ShipRocketApi.php
+++ app/Services/ShippingService/Providers/ShipRocket/ShipRocketApi.php
@@ -7,33 +7,50 @@ class ShipRocketApi
 {
 
     protected string $apiBase = 'https://apiv2.shiprocket.in/v1/external/';
-    protected string $apiUrl =  'https://apiv2.shiprocket.in/v1/external/auth/login';
-    protected string $apiToken;
+    protected string $token;
 
 
-    public function __construct(string $api_key,string $api_secret)
+    public function __construct(string $email,string $password)
     {
-
-        $this->apiToken = Http::post($this->apiUrl, [
-            'email' => $api_key,
-            'password' => $api_secret,
-        ])['token'];
-    }
-
-    public function http(): PendingRequest
-    {
-        return Http::withToken($this->apiToken)->contentType('application/json');
+        $loginData = $this->login([
+            'email' => $email,
+            'password' => $password
+        ]);
+        throw_unless($loginData['token'],'Shiprocket Authentication Failed! check credentials');
+        $this->token = $loginData['token'];
     }
 
     public function getToken():string
     {
-        return $this->apiToken;
+        return $this->token;
     }
 
     public function getBaseUrl(): string
     {
         return $this->apiBase;
     }
+
+    protected function login(array $credentials)
+    {
+        $response = Http::post($this->getBaseUrl()."auth/login", [
+            'email' => $credentials['email'],
+            'password' => $credentials['password'],
+        ]);
+        throw_if($response->failed(),$response->body());
+        return $response->json();
+    }
+
+
+
+
+
+    public function http(): PendingRequest
+    {
+        return Http::withToken($this->token)->retry(3, 100);
+        //return Http::withToken($this->apiToken)->contentType('application/json');
+    }
+
+
 
 
 
