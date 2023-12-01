78088fc0ed7f558c65052cdbc35c7983c37f5e3a
iotronlab <50877415+iotronlab@users.noreply.github.com>
2023-12-01 22:49:59 +0530

    Update ListThemes.php

--- app/Filament/Resources/Category/ThemeResource/Pages/ListThemes.php
+++ app/Filament/Resources/Category/ThemeResource/Pages/ListThemes.php
@@ -5,6 +5,17 @@ namespace App\Filament\Resources\Category\ThemeResource\Pages;
 use App\Filament\Resources\Category\ThemeResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\Category\ThemeResource\Pages;
+use App\Models\Category\Theme;
+use Filament\Forms;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ListThemes extends ListRecords
 {
@@ -15,5 +26,30 @@ class ListThemes extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('url'),
+            ])
+            ->filters([
+                SelectFilter::make('Parent')
+                    ->relationship('parent', 'name')
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
     }
 }
