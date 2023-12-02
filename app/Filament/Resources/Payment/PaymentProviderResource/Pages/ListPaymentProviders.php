ba47fc9d40cac45f325a977bafe85ec9899d68d8
iotronlab <50877415+iotronlab@users.noreply.github.com>
2023-12-02 19:17:13 +0530

    uploaded composer

--- app/Filament/Resources/Payment/PaymentProviderResource/Pages/ListPaymentProviders.php
+++ app/Filament/Resources/Payment/PaymentProviderResource/Pages/ListPaymentProviders.php
@@ -18,86 +18,27 @@ class ListPaymentProviders extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
         ];
     }
-
-
-
-
 
     public  function table(Table $table): Table
     {
         return $table
             ->columns([
+                Tables\Columns\Layout\Stack::make([
+                    Tables\Columns\Layout\Split::make([ Tables\Columns\TextColumn::make('name')
+                    ->size(Tables\Columns\TextColumn\TextColumnSize::Large)
+                    ->weight(FontWeight::Bold),
+                    Tables\Columns\IconColumn::make('status')
+                    ->tooltip('Provider Status')
+                    ->boolean()->alignRight(),]),
 
+                    Tables\Columns\IconColumn::make('is_primary')
+                    ->boolean()
+                    ->tooltip('Is Primary Provider?')
+                    ->alignRight(),
 
-                Tables\Columns\Layout\Split::make([
-                    Tables\Columns\TextColumn::make('name')
-                        ->size(Tables\Columns\TextColumn\TextColumnSize::Large)
-                        ->weight(FontWeight::Bold)
-                        ->searchable(),
-                    Tables\Columns\TextColumn::make('code')
-                        ->alignRight()
-                        ->badge()
-                        ->searchable(),
-
-                ])->grow(),
-
-                Tables\Columns\Layout\Split::make([
-                    Tables\Columns\Layout\Stack::make([
-                        Tables\Columns\TextColumn::make('service_provider')
-                            ->tooltip('Service Provider')
-                            ->formatStateUsing(function ($state){
-                                return Str::remove('App\Services\PaymentService',$state);
-                            })
-                            ->grow()
-                            ->weight(FontWeight::Thin)
-                            ->searchable(),
-                        Tables\Columns\Layout\Split::make([
-                            Tables\Columns\TextColumn::make('payments_count')->counts('payments')
-                                ->description('Payments')
-                                ->tooltip('Total Payments Init')
-                                ->weight(FontWeight::Bold),
-//                            Tables\Columns\TextColumn::make('orders_count')->counts('orders')
-//                                ->description('Orders')
-//                                ->tooltip('Total Orders Init')
-//                                ->weight(FontWeight::Bold),
-
-                        ]),
-                    ]),
-
-                    Tables\Columns\Layout\Stack::make([
-                        Tables\Columns\IconColumn::make('is_primary')
-                            ->boolean()
-                            ->tooltip('Is Primary Provider?')
-                            ->alignRight(),
-                        Tables\Columns\IconColumn::make('has_api')
-                            ->tooltip('Is Provider has Api')
-                            ->boolean()->alignRight(),
-                        Tables\Columns\IconColumn::make('status')
-                            ->tooltip('Provider Status')
-                            ->boolean()->alignRight(),
-                    ])
                 ]),
-
-
-
-
-                Tables\Columns\Layout\Split::make([
-                    Tables\Columns\TextColumn::make('created_at')
-                        ->dateTime()
-                        ->sortable()
-                        ->description('Created On')
-                        ->toggleable(isToggledHiddenByDefault: true),
-                    Tables\Columns\TextColumn::make('updated_at')
-                        ->dateTime()
-                        ->sortable()
-                        ->alignRight()
-                        ->description('Update On')
-                        ->toggleable(isToggledHiddenByDefault: true),
-                ]),
-
 
 
 
@@ -113,9 +54,7 @@ class ListPaymentProviders extends ListRecords
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+
             ]);
     }
 
