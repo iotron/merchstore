e1d4233b66ba31524967cfe593e35f2fed2ef5b9
Krishanu <36711704+krishzzi@users.noreply.github.com>
2024-11-02 22:23:06 +0530

    update

--- app/Http/Resources/Product/ProductIndexResource.php
+++ app/Http/Resources/Product/ProductIndexResource.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Resources\Product;
 
-use App\Services\Iotron\MoneyService\Money;
+
+use App\Services\MoneyServices\Money;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
